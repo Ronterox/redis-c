d2e72d64c40405a8f34a10115ec73bb3b2198e48
Ricardo <ricarditomontserrat@gmail.com>
2024-03-25 02:46:25 +0100

    Removed some comments

--- spawn_redis_server.sh
+++ spawn_redis_server.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
-#
-# DON'T EDIT THIS!
-#
-# CodeCrafters uses this file to test your code. Don't make any changes here!
-#
-# DON'T EDIT THIS!
 set -e
 tmpFile=$(mktemp)
-gcc app/*.c -o $tmpFile
+gcc app/*.c -o "$tmpFile"
 exec $tmpFile "$@"
