640b1ce11935a3678febba1276c198077c611712
Ricardo <ricarditomontserrat@gmail.com>
2024-04-01 23:59:28 +0200

    Removed received

--- app/server.c
+++ app/server.c
@@ -238,7 +238,6 @@ void *handle_client(void *args) {
 		if (read(client_fd, buffer, BUFFER_SIZE) <= 0)
 			break;
 
-		printf("Received: %s\n", buffer);
 		char *data = strtok(strdup(buffer), "\r\n");
 		do {
 			if (data[0] == '*') {
